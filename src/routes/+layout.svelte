6527146d6492df6e6b4f29162e7a710577865f81
Heyuan Zeng <hi@universe.observer>
2026-05-13 18:05:11 +0100

    Remove website icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/Header.svelte';
 
 	const { data, children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <div class="app">
 	<Header quizIds={data.quizIds} />
